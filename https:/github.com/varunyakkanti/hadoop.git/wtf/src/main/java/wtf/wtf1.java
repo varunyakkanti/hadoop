d0ca7860c56f71fc4235e9a4801a960cb910e441
varunyakkanti <yakkantivarunreddy@gmail.com>
2017-02-12 20:00:51 -0500

    final

--- https:/github.com/varunyakkanti/hadoop.git/wtf/src/main/java/wtf/wtf1.java
+++ https:/github.com/varunyakkanti/hadoop.git/wtf/src/main/java/wtf/wtf1.java
@@ -1,58 +1,35 @@
 package wtf;
 
-
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.StringTokenizer;
+import java.util.function.Predicate;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.chain.Chain;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class wtf1 {
 
 	private static final String OUTPUT_PATH = "intermediate_output42";
     /********************/
-    /**    Mapper      **/
+    /**    Mapper  2    **/
     /********************/
-    
-    public static class AllPairsMapper extends Mapper<Object,Text, IntWritable, IntWritable> {
+	    public static class SecondMapper extends Mapper<Object , Text, IntWritable, IntWritable> {
         // Emits (a,b) *and* (b,a) any time a friend common to a and b is found.
         public void map(Object key, Text values, Context context) throws IOException, InterruptedException {
-            // Key is ignored as it only stores the offset of the line in the text file
-            StringTokenizer st = new StringTokenizer(values.toString());
-            // seenFriends will store the friends we've already seen as we walk through the list of friends
-            ArrayList<Integer> seenFriends = new ArrayList<Integer>(); 
-            // friend1 and friend2 will be the elements in the emitted pairs.
-            IntWritable friend1 = new IntWritable();
-            IntWritable friend2 = new IntWritable();
-            friend1.set(Integer.parseInt(st.nextToken()));
-            //st.nextToken(); // discards first token (key)
-            while (st.hasMoreTokens()) {
-                // For every friend Fi found in the values,
-                // we emit (Fi,Fj) and (Fj,Fi) for every Fj in the 
-                // friends we have seen before. You can convince yourself
-                // that this will emit all (Fi,Fj) pairs for i!=j.
-                
-            	friend2.set(Integer.parseInt(st.nextToken()));
-                    context.write(friend2, friend1);
-                    
-              System.out.println(friend2+"  "+friend1);
-            }
-        }
-    }
-    public static class AllPairsMapper1 extends Mapper<Object, Text, IntWritable, IntWritable> {
-        // Emits (a,b) *and* (b,a) any time a friend common to a and b is found.
-    	public void map(Object key, Text values, Context context) throws IOException, InterruptedException {
             // Key is ignored as it only stores the offset of the line in the text file
             StringTokenizer st = new StringTokenizer(values.toString());
             // seenFriends will store the friends we've already seen as we walk through the list of friends
@@ -95,37 +72,42 @@ public class wtf1 {
             }
         }
     }
+	    /********************/
+	    /**    Mapper  1    **/
+	    /********************/
+    public static class FirstMapper extends Mapper<Object, Text, IntWritable, IntWritable> {
+         // Inverts the array of input followers to follwed by list
+        public void map(Object key, Text values, Context context) throws IOException, InterruptedException {
+            // Key is ignored as it only stores the offset of the line in the text file
+            StringTokenizer st = new StringTokenizer(values.toString());
+            // seenFriends will store the friends we've already seen as we walk through the list of friends
+            ArrayList<Integer> seenFriends = new ArrayList<Integer>(); 
+           
+            IntWritable friend1 = new IntWritable();
+            IntWritable key1 = new IntWritable();
+ 			IntWritable tempfriend = new IntWritable();
+            key1.set(Integer.parseInt(st.nextToken())); 
+             
+             while (st.hasMoreTokens()) {
+ 				String s =st.nextToken();
+                 friend1.set(Integer.parseInt(s));
+                 //Emit the list of existing frens as negetive values so it can be ignored in final recomended list
+                 tempfriend.set(- Integer.parseInt(s));
+                     context.write(friend1,key1);
+                     context.write(key1, tempfriend);
+                     System.out.println("pos"+key1+","+friend1);
+                     System.out.println("Neg"+key1+","+tempfriend);
+             }
 
-    /**********************/
-    /**      Reducer     **/
-    /**********************/
-    
-    public static class CountReducer extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
-
-        
-        // The reduce method
-        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            // user stores the id of the user for which we are searching for recommendations
-            IntWritable user = key;
-            // recommendations will store all the recommendations for user 'user'
-            ArrayList<Integer> recommendations = new ArrayList<Integer>();
-            // Builds the recommendation array
-            while (values.iterator().hasNext()) {
-                int userWithCommonFriend = values.iterator().next().get();
-               
-                    recommendations.add(userWithCommonFriend);
-               
-            
         }
-            String str = recommendations.toString();
-            Text t1=new Text(str);
-            System.out.println("----------------------recommendations--------------------------");
-            context.write(user, t1);
     }
-    }
-    public static class CountReducer1 extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
 
-    	// A private class to describe a recommendation.
+    /**********************/
+    /**      Reducer  2   **/
+    /**********************/
+    public static class SecondReducer extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
+
+        // A private class to describe a recommendation.
         // A recommendation has a friend id and a number of friends in common.
         private static class Recommendation {
 
@@ -227,7 +209,85 @@ public class wtf1 {
             context.write(user, result);
         }
     }
-    
+
+    /**********************/
+    /**      Reducer  1   **/
+    /**********************/
+		
+    public static class FirstReducer extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
+
+        // A private class to describe a recommendation.
+        // A recommendation has a friend id and a number of friends in common.
+        private static class Recommendation {
+            
+            // Attributes
+            private int friendId;
+            private int nCommonFriends;
+            
+            // Constructor
+            public Recommendation(int friendId) {
+                this.friendId = friendId;
+                // A recommendation must have at least 1 common friend
+                this.nCommonFriends = 1;
+            }
+            // Getters
+            public int getFriendId() {
+                return friendId;
+            }
+            public int getNCommonFriends() {
+                return nCommonFriends;
+            }
+
+            // Other methods
+            // Increments the number of common friends
+            public void addCommonFriend() {
+                nCommonFriends++;
+            }
+            // String representation used in the reduce output            
+            public String toString() {
+                return friendId+"("+nCommonFriends+")";
+            }
+            // Finds a representation in an array
+            public static Recommendation find(int friendId, ArrayList<Recommendation> recommendations) {
+                for (Recommendation p : recommendations)
+                    if (p.getFriendId() == friendId)
+                        return p;
+                // Recommendation was not found!
+                return null;
+            }
+        }
+
+        // The reduce method
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            // user stores the id of the user for which we are searching for recommendations
+            IntWritable user = key;
+            Text result = null;
+            String s = null;
+            // recommendations will store all the recommendations for user 'user'
+            ArrayList<Recommendation> recommendations = new ArrayList<Recommendation>();
+            // Builds the recommendation array
+            Integer i = 0;
+            while (values.iterator().hasNext()) {
+            	Integer sb = values.iterator().next().get();
+            	
+            	if(i==0) {
+            		s =sb.toString();
+            		System.out.println("firstiter"+s);
+            		
+            	}else{
+            		s =s+" "+sb.toString();
+            		System.out.println("seconiterator"+s);
+            	}
+            	
+            	i++ ;	        	
+            }
+            System.out.println(s);
+            result = new Text(s.toString());
+            context.write(user, result);
+
+        }    
+        }
+		
 
     public static int main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
     	//First set of mapper/reducer
@@ -235,8 +295,8 @@ public class wtf1 {
         Job job = Job.getInstance(conf, "people you may know");
 	//	job.setJarByClass(Chain.class);
         job.setJarByClass(wtf1.class);
-        job.setMapperClass(AllPairsMapper.class);
-        job.setReducerClass(CountReducer.class);
+        job.setMapperClass(FirstMapper.class);
+        job.setReducerClass(FirstReducer.class);
         job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
@@ -248,8 +308,8 @@ public class wtf1 {
         Job job2 = new Job(conf2, "Job 2");
         job2.setJarByClass(wtf1.class);
         
-        job2.setMapperClass(AllPairsMapper1.class);
-        job2.setReducerClass(CountReducer.class);
+        job2.setMapperClass(SecondMapper.class);
+        job2.setReducerClass(SecondReducer.class);
         
         job2.setOutputKeyClass(IntWritable.class);
         job2.setOutputValueClass(IntWritable.class);
