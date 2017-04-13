92039a47123b3c165341c30acb86a7e00e50bbe0
varunyakkanti <yakkantivarunreddy@gmail.com>
2017-04-12 20:54:07 -0400

    question4

---
+++ assignment2/ass/ques4.py
@@ -0,0 +1,42 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql.functions import size
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
+from pyspark.sql import Row
+import os
+import sys
+
+def parseLogLine(logline):
+    if 'Started' in logline:
+        i=logline.index('user')
+        return str(logline[i+1])
+    
+# Configure the Spark environment
+sparkConf = SparkConf().setAppName("WordCounts").setMaster("local")
+sc = SparkContext(conf = sparkConf)
+
+# The WordCounts Spark program
+tfile1=sys.argv[1]
+tfile=sys.argv[2]
+val1="Started Session"
+val2="of user"
+
+textFile = sc.textFile(tfile1)
+filter11=textFile.filter(lambda values:val1 in values)
+filter12=filter11.filter(lambda values:val2 in values)
+filter13=filter12.map(lambda b:b.split())
+filter14=filter13.map(lambda a: str(parseLogLine(a)))
+filter15=filter14.map(lambda x:(x,1))
+filter16=filter15.reduceByKey(lambda a,b:a+b)
+
+for wc in filter16.collect(): print wc
+
+
+
+
+textFile2 = sc.textFile(tfile)
+filter21= textFile2.filter(lambda word:val1 in word)
+filter22=filter21.filter(lambda word:val2 in word).map(lambda b:b.split())
+filter23=filter22.map(lambda a: str(parseLogLine(a)))
+filter24=filter23.map(lambda x:(x,1))
+filter25=filter24.reduceByKey(lambda a,b:a+b)
+for wc in filter25.collect(): print wc
